c1fc1111345d81b7e1258b90b3419b6091c89af7
nabila2009 <142683997+nabila2009@users.noreply.github.com>
2023-09-06 22:17:33 +0300

    Update main.py
    day 6

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-# write your code here
+person={
+    "name":"nabila" ,
+    "age":"14",
+    "hobbies":["running","sleeping","coding"]
+} 
+print(person.get ("name"))
+print(person.get("age"))
+person["age"]=15
+person["country"]="yemen"
+print(person)
+print(len(person))
+person["hobbies"]="eating"
+def check_hobbies(person):
+  if len( person["hobbies"])>=3:
+    print("wow you are amazing") 
+check_hobbies(person)
+
